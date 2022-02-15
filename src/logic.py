bc7df92709b560ddfa3a3238f8ae249dc29d1b8b
samjamramman <samjamramman@gmail.com>
2022-02-15 23:26:52 +0000

    test 2 complete

--- src/logic.py
+++ src/logic.py
@@ -22,5 +22,11 @@ def get_useful_datasets():
     return usefulDatasets
 
 def get_latest_taxes_and_benefits_url():
-  # This method must 1. call get_useful_datasets() 2. search for the data set where 'id' is 'tax-benefits-statistics' 3. return the value in .links.lastest.href
-  return "" # to be completed...
+  # This method must 1. call get_useful_datasets()
+  datasets = get_useful_datasets()
+  #  2. search for the data set where 'id' is 'tax-benefits-statistics' 
+  for dataset in datasets:
+    if dataset["id"] == "tax-benefits-statistics":
+      correctDataset = dataset
+  # 3. return the value in .links.lastest.href
+  return correctDataset["links"]["latest_version"]["href"]
