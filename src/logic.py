e53f88faebf4b69345020cbd9874b189e31ff569
samjamramman <samjamramman@gmail.com>
2022-02-15 23:38:14 +0000

    improved functions

--- src/logic.py
+++ src/logic.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-def get_useful_datasets():
+def get_pay_datasets():
     
     #gets the list of datasets
     datasets = requests.get("https://api.beta.ons.gov.uk/v1/datasets?limit=45")
@@ -21,12 +21,12 @@ def get_useful_datasets():
           usefulDatasets.append(dataset)
     return usefulDatasets
 
-def get_latest_taxes_and_benefits_url():
+def get_latest_url_by_id(id):
   # This method must 1. call get_useful_datasets()
-  datasets = get_useful_datasets()
+  datasets = get_pay_datasets()
   #  2. search for the data set where 'id' is 'tax-benefits-statistics' 
   for dataset in datasets:
-    if dataset["id"] == "tax-benefits-statistics":
+    if dataset["id"] == id:
       correctDataset = dataset
   # 3. return the value in .links.lastest.href
   return correctDataset["links"]["latest_version"]["href"]
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,9 +1,9 @@
 import src.logic as logic
 
 def test_taxes_and_benefits_are_found():
-    datasets = logic.get_useful_datasets()
+    datasets = logic.get_pay_datasets()
     assert "Taxes and Benefits" in datasets[0]["title"]
 
 def test_find_taxes_and_benefits_lastest_url():
-    latest = logic.get_latest_taxes_and_benefits_url()    
+    latest = logic.get_latest_url_by_id("tax-benefits-statistics")    
     assert "tax-benefits-statistics/editions/time-series/versions" in latest
