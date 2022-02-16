bb90a13dd2313014bea40432b202329ae3cbee39
samjamramman <samjamramman@gmail.com>
2022-02-16 11:57:00 +0000

    improved functions again

--- src/logic.py
+++ src/logic.py
@@ -1,11 +1,16 @@
 import json
 import requests
 
+
+def get_datasets():
+  datasets = requests.get("https://api.beta.ons.gov.uk/v1/datasets?limit=45")
+  data = json.loads(datasets.text)
+  return data
+
 def get_pay_datasets():
     
     #gets the list of datasets
-    datasets = requests.get("https://api.beta.ons.gov.uk/v1/datasets?limit=45")
-    data = json.loads(datasets.text)
+    datasets = get_datasets()
     
     payDatasets=[]
     
@@ -23,10 +28,17 @@ def get_pay_datasets():
 
 def get_latest_url_by_id(id):
   # This method must 1. call get_useful_datasets()
-  datasets = get_pay_datasets()
+  datasets = get_datasets()
   #  2. search for the data set where 'id' is 'tax-benefits-statistics' 
-  for dataset in datasets:
+  for dataset in datasets["items"]:
     if dataset["id"] == id:
       correctDataset = dataset
   # 3. return the value in .links.lastest.href
   return correctDataset["links"]["latest_version"]["href"]
+
+def get_dataset_by_id(id):
+  datasets = get_datasets()
+  for dataset in datasets["items"]:
+    if dataset["id"] == id:
+      correctDataset = dataset
+  return correctDataset
